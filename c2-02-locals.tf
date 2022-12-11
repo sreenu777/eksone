155826d52ba87536eb74012f19db97321583e88f
Palepu Venkata Srinivasa varma <palepu.v@intimetec.com>
2022-12-11 20:21:24 +0530

    eks-1

---
+++ c2-02-locals.tf
@@ -0,0 +1,12 @@
+# Define Local Values in Terraform
+locals {
+  owners = var.business_divsion
+  environment = var.environment
+  name = "${var.business_divsion}-${var.environment}"
+  common_tags = {
+    owners = local.owners
+    environment = local.environment     
+  }
+  # Add additional local value
+  eks_cluster_name = "${local.name}-${var.cluster_name}"  
+}
